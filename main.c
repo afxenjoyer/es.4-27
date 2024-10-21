a742dcd6f180cdbd12b03338904f9e8a4ed81618
Andrea Di Mingo <dimingo.andrea25@gmail.com>
2024-10-21 20:57:03 +0200

    Corretto il programma per l'esercizio 4.27

--- main.c
+++ main.c
@@ -1,35 +1,24 @@
 #include <stdio.h>
-#include <math.h>
 
 int main(void)
 {
-    int i;
     int numeroDaScomporre;
-    int fattori = 0;
+    int divisore = 2;
 
     printf("Inserisci il numero da scomporre\n");
     scanf("%d", &numeroDaScomporre);
 
-    if (numeroDaScomporre > -1)
+    while (numeroDaScomporre != 1)
     {
-        while(numeroDaScomporre % 2 == 0)
+        if (numeroDaScomporre % divisore == 0)
         {
-            numeroDaScomporre = numeroDaScomporre / 2;
-            printf("2\t");
+            printf("%d ", divisore);
+            numeroDaScomporre = numeroDaScomporre / divisore;
         }
-        for (i = 3; i <= sqrt(numeroDaScomporre) && numeroDaScomporre == 1; i+=2)
+        else
         {
-            while(numeroDaScomporre % i == 0)
-            {
-                numeroDaScomporre = numeroDaScomporre / i;
-                fattori = i;
-                printf("%d\t", fattori);
-            }
+            divisore++;
         }
-    }
-    else
-    {
-        printf("Il numero da scomporre è invalido\n");
     }
 
     return 0;
